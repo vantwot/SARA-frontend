587409857027b685f2ad043e2c63489300d9594c
KPOETA1 <geider.montano@correounivalle.edu.co>
2023-06-08 21:11:43 -0500

    Pruebas perfil

---
+++ src/test/a.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Login from '../pages/login-register/Login.jsx';
+
+
+
+test('submit form should navigate to the student dashboard on successful login', () => {
+  const { getByPlaceholderText, getByText, getByTestId } = render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  );
+
+  const usernameInput = getByPlaceholderText('Codigo de identificacion');
+  const passwordInput = getByPlaceholderText('Contraseña');
+  const submitButton = getByText('Iniciar sesión');
+
+  fireEvent.change(usernameInput, { target: { value: '1863917' } });
+  fireEvent.change(passwordInput, { target: { value: 'kpoeta2015' } });
+
+  fireEvent.click(submitButton);
+
+
+
+});
+
+test('submit form should navigate to the professor dashboard on successful login', () => {
+  const { getByPlaceholderText, getByText, getByTestId } = render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  );
+
+  const usernameInput = getByPlaceholderText('Codigo de identificacion');
+  const passwordInput = getByPlaceholderText('Contraseña');
+  const submitButton = getByText('Iniciar sesión');
+
+  fireEvent.change(usernameInput, { target: { value: '1871952' } });
+  fireEvent.change(passwordInput, { target: { value: 'password' } });
+
+  fireEvent.click(submitButton);
+
+    // espera que se redirija a la pagina del estudiante
+    expect(global.window.location.pathname).toContain('/'); 
+  
+});
