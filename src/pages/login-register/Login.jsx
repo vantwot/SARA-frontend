f277f37b08c22bf4500beee359090995a229ea30
Van <45314803+vantwot@users.noreply.github.com>
2023-06-08 19:35:06 -0500

    cambio de login,primera prueba del front
    Cambios realizados por Geider en el computador de Valentina

--- src/pages/login-register/Login.jsx
+++ src/pages/login-register/Login.jsx
@@ -3,6 +3,8 @@ import "../../styles/logreg.css";
 import univalle from "./Univalle.svg.png";
 import { useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = (props) => {
   const [text, setText] = useState("Iniciar sesión");
@@ -68,7 +70,9 @@ const Login = (props) => {
       // navigate("home/", {state:userInfo});
     } catch (error) {
       console.log(error);
+      toast.error("Ha ocurrido un error al iniciar sesión")
       setError("Ha ocurrido un error al iniciar sesión");
+      setText("Iniciar Sesión");
     }
     console.log(error);
   };
@@ -173,6 +177,8 @@ const Login = (props) => {
           </div >
         </div >
       </form >
+      <div id="login-error-message" />
+      <ToastContainer />
     </div >
     </center>
   );
